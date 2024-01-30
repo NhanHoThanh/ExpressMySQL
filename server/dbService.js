91a2a5d7777de7635c76d0ad67a2106bd8b4c63d
NhanHoThanh <hothanhnhanhot@gmail.com>
2024-01-30 11:39:06 +0000

    oke

--- server/dbService.js
+++ server/dbService.js
@@ -4,11 +4,11 @@ let instance = null;
 dotenv.config();
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.DB_PORT
+    host: "localhost",
+    user: "root",
+    password: "new_password",
+    database: "namedb",
+    port: 3306
 });
 
 connection.connect((err) => {
